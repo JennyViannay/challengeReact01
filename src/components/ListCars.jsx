468a470088fae2eeb299b4e911eaefce18f8da5e
JennyViannay <jenny.viannay75@gmail.com>
2019-11-18 13:24:22 +0100

    add change view with available cars

--- src/components/ListCars.jsx
+++ src/components/ListCars.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 import Car from "./Car";
 import axios from "axios";
+import './style.css';
+import InputRange from 'react-input-range';
+import 'react-input-range/lib/css/index.css';
 
 class ListCars extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            cars: [],
-            carsAvailibility: [],
-            bestCarsOnly: false,
-            isLoaded: false
-          };
-      }
-  
+  constructor() {
+    super()
+    this.state = {
+      cars: [],
+      carsAvailibility: [],
+      bestCarsOnly: false,
+      isLoaded: false,
+      valueDay: 10,
+      valueKm: 50,
+    };
+  }
 
-  getCars ()  {
+
+  getCars() {
     axios
       .get(`http://localhost:3001/cars.json`)
       .then(response => response.data)
       .then(data => {
-        this.setState({ cars: data , isLoaded: true});
+        this.setState({ cars: data, isLoaded: true });
       });
   };
 
@@ -32,51 +37,89 @@ class ListCars extends React.Component {
     this.setState({ cars: this.state.cars });
   };
 
-  bestcarswitch() {
-    this.setState({ bestcarsOnly: !this.state.bestcarsOnly });
+  bestcarswitch(a,b) {
+    this.setState({ bestCarsOnly: !this.state.bestCarsOnly });
+    const cars = this.state.cars
+    const newArray = [];
+    for(let i=0; i < cars.length; i++){
+      if(cars[i].availability.maxDuration <= a && cars[i].availability.maxDistance <= b){
+        console.log(cars[i])
+        newArray.push(cars[i])
+        
+      } 
+    }
+    this.setState({carsAvailibility : newArray})
+    console.log(this.state.carsAvailibility)
   }
 
-  getCarWithAvailibility() {
-    const availibility = [];
-      this.state.cars.map((car, i = 0) => {
-          availibility.push(car.availability)
-      })
-      const array = [];
-      return availibility.filter(car=> car.maxDuration <= 30 )
-       
+  renderData(){
+    const allCars = this.state.cars
+      .map((car, i) => (
+        <Car key={car.id}
+          id={car.id}
+          brand={car.brand}
+          model={car.model}
+          pricePerDay={car.pricePerDay}
+          pricePerKm={car.pricePerKm}
+          index={i}
+          delete={() => this.deleteCar(i)}
+        />
+      ))
+
+    const carsAvailable = this.state.carsAvailibility
+    .map((car, i) => (
+      <Car key={car.id}
+        id={car.id}
+        brand={car.brand}
+        model={car.model}
+        pricePerDay={car.pricePerDay}
+        pricePerKm={car.pricePerKm}
+        index={i}
+        delete={() => this.deleteCar(i)}
+      />
+    ))
+
+    if(this.state.isLoaded && this.state.bestCarsOnly == false){
+      return allCars;
+    }
+    if(this.state.isLoaded && this.state.bestCarsOnly == true){
+      return carsAvailable;
+    }
   }
 
   render() {
-    console.log(this.getCarWithAvailibility())
+    console.log(this.state.carsAvailibility)
+    
     return (
       <>
-        {/* <Link className="title" to="/">
-          <h1>Home</h1>
-        </Link> */}
+        <div className="carsGlobal">
+        
+          <InputRange
+            maxValue={30}
+            minValue={1}
+            formatLabel={value => `${value} jours`}
+            value={this.state.valueDay}
+            onChange={value => this.setState({ valueDay: value })} />
 
-        <button className="RatingButton" onClick={() => this.bestcarswitch()}>
-          {this.state.bestcarsOnly ? "All cars" : "Best cars Only"}
+          <InputRange
+            maxValue={10000}
+            minValue={50}
+            step={50}
+            formatLabel={value => `${value} km`}
+            value={this.state.valueKm}
+            onChange={value => this.setState({ valueKm: value })} />
+        </div>
+
+        <button 
+          className="RatingButton" 
+          onClick={() => this.bestcarswitch(this.state.valueDay, this.state.valueKm)}>
+            {this.state.bestCarsOnly ? "All cars" : "Best cars Only"}
         </button>
 
         <div className="carsGlobal">
-          {this.state.isLoaded ? (
-            this.state.cars
-              .filter(car => !this.state.bestcarsOnly || car.rating >= 4.5)
-              .map((car, i) => (
-                <Car key={car.id}
-                  id={car.id}
-                  brand={car.brand}
-                  model={car.model}
-                  pricePerDay={car.pricePerDay}
-                  pricePerKm={car.pricePerKm}
-                  index={i}
-                  delete={() => this.deleteCar(i)}
-                />
-              ))
-          ) : (
-            <p>Loading...</p>
-          )}
+        {this.renderData()}
         </div>
+        
       </>
     );
   }
